5c797f3397d135501a8a4cb6c65ef155f537c49c
Debajyoti Debnath <debajyotidebnath43783@gmail.com>
2024-09-19 13:41:33 +0530

    routes

--- backend/app.js
+++ backend/app.js
@@ -8,11 +8,13 @@ app.use(cookieParser())
 //route imports
 const assignmentRoute=require('./routes/assignmentRoute.js')
 const userRoute= require('./routes/userRoute')
+const classRoute=require('./routes/classRoute.js')
 // const orderRoute= require('./routes/orderRoute')
 
 // app.use("/api/v1", productRoute)
 app.use('/api/v1', userRoute)
 app.use('/api/v1', assignmentRoute)
+app.use('/api/v1', classRoute)
 //middleware for error
 
 app.use(errorMiddleware)
