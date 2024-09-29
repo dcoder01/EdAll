bc29f370b31486b3e96f034060f6f44d4a719d09
Debajyoti Debnath <debajyotidebnath43783@gmail.com>
2024-09-29 13:46:34 +0530

    route cleanup with index.js

--- backend/app.js
+++ backend/app.js
@@ -6,18 +6,8 @@ app.use(express.json())
 app.use(cookieParser())
 
 //route imports
-const assignmentRoute=require('./routes/assignmentRoute.js')
-const userRoute= require('./routes/userRoute')
-const classRoute=require('./routes/classRoute.js')
-const announcementRoute=require('./routes/announcementRoute.js')
-const quizzeRoute=require('./routes/quizRoute.js')
 
-app.use('/api/v1', userRoute)
-app.use('/api/v1', assignmentRoute)
-app.use('/api/v1', classRoute)
-app.use('/api/v1/announcement', announcementRoute)
-app.use('/api/v1/quiz', quizzeRoute)
-// app.use('/api/v1', require("./routes"))
+app.use('/api/v1', require("./routes/index.js"))
 //middleware for error
 
 app.use(errorMiddleware)
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,13 +1,14 @@
-// const express = require("express");
-// const router = express.Router();
+const express = require("express");
+const router = express.Router();
 
-// // router.use("/login", require("./login"));
-// router.use("/class", require("./classRoute"));
-// // router.use("/quiz", require("./quiz"));
-// router.use("/assignment", require("./assignmentRoute"));
-// // router.use("/announcement", require("./announcement"));
-
-// module.exports = router;
+router.use("/user", require("./userRoute"));
+router.use("/class", require("./classRoute"));
+router.use("/quiz", require("./quizRoute"));
+router.use("/assignment", require("./assignmentRoute"));
+router.use("/announcement", require("./announcementRoute"));
 
 
-//TODO:future segregation
+module.exports = router;
+
+
+// TODO:future segregation
