31cf7684a6c8dcbeb70a2b3e4fc7f14ed6590b95
Debajyoti Debnath <debajyotidebnath43783@gmail.com>
2024-12-09 23:06:42 +0530

    bug fix--> alert text color

--- frontend/src/components/common/Alert.jsx
+++ frontend/src/components/common/Alert.jsx
@@ -6,7 +6,7 @@ export default function Alert({ color, message }) {
       style={{
         fontFamily: ["Sen", "sans-serif"],
       }}
-      className={`text-lg font-semibold px-2 py-2 border rounded relative mb-4 bg-${color}-500 text-white`}
+      className={`text-lg font-semibold px-2 py-2 border rounded relative mb-4 bg-${color}-500`}
     >
       <span className="inline-block align-middle mr-8 break-words break-all">
         {message}
