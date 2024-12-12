519cb8b72591404261d10c0a4c22463e816a8c5d
Debajyoti Debnath <debajyotidebnath43783@gmail.com>
2024-12-13 01:06:18 +0530

    token creation for videoSdk-live

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,12 @@
 const express= require('express')
-const app=express()
+const cors = require("cors");
 const cookieParser=require('cookie-parser')
 const errorMiddleware= require('./middleware/error.js')
+const app=express()
+app.use(cors());
 app.use(express.json())
 app.use(cookieParser())
-
+app.use(express.urlencoded({ extended: true }));
 //route imports
 
 app.use('/api/v1', require("./routes/index.js"))
--- backend/server.js
+++ backend/server.js
@@ -1,50 +1,60 @@
-
-const app= require('./app');
+const http = require('http');
+const app = require('./app');
+const connectDatabase = require('./config/database');
 require("dotenv").config();
+const jwt = require("jsonwebtoken");
+const server = http.createServer(app);
 
-const connectDatabase=require('./config/database')
 
-//handling uncaught exception
+app.get("/get-token", (req, res) => {
+  const API_KEY = process.env.VIDEOSDK_API_KEY;
+  const SECRET_KEY = process.env.VIDEOSDK_SECRET_KEY;
+  const options = { expiresIn: "10m", algorithm: "HS256" };
+  let payload = {
+      apikey: API_KEY,
+      permissions: ["allow_join", "allow_mod","allow_screen_share"],
+     
+  };
+  const token = jwt.sign(payload, SECRET_KEY, options);
+  res.json({ token });
+});
 
-process.on("uncaughtException", err=>{
+process.on("uncaughtException", err => {
     console.log(`Error: ${err.message}`);
     console.log(`shutting down the server due to uncaught exception`);
     process.exit(1)
-    
+
 })
 
-// console.log(youtube);
-
-//config
-// dotenv.config({path: 'config/config.env'})
-
-//connecting database
-connectDatabase(process.env.MONGO_URL)
-.then((data)=>{
-    console.log(`mongodb connected with server: ${data.connection.host}`);
-    
-})
-// .catch((err)=>{
-//     console.log(err);
-    
-// }) 
-
-const server=app.listen(process.env.PORT, ()=>{
-    console.log("server is working", process.env.PORT);
-    
-})
 
 
 //unhandled promise rejection
-
-process.on('unhandledRejection',err=>{
+process.on('unhandledRejection', err => {
     console.log(`Error: ${err.message}`);
     console.log(`shutting down the server due to unhandled promise rejection`);
 
 
-    server.close(()=>{
+    server.close(() => {
         process.exit(1)
     })
     
-    
+
 })
+
+//connecting database
+connectDatabase(process.env.MONGO_URL)
+    .then((data) => {
+        console.log(`mongodb connected with server: ${data.connection.host}`);
+
+    })
+// .catch((err)=>{
+//     console.log(err);
+
+// }) 
+
+ server.listen(process.env.PORT, () => {
+    console.log("server is working", process.env.PORT);
+
+})
+
+
