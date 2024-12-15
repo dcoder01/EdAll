acbb58d4a6f68427ef5d8362e68d4853fc4377f3
Debajyoti Debnath <debajyotidebnath43783@gmail.com>
2024-12-15 20:56:50 +0530

    added env and config change for production

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,11 +4,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-    proxy:{
-      '/api/v1':'http://localhost:4000',
-    },
-  },
+  // server:{
+  //   proxy:{
+  //     '/api/v1':'http://localhost:4000',
+  //   },
+  // },
   plugins: [react()],
   resolve: {
     alias: {
@@ -16,3 +16,7 @@ export default defineConfig({
     },
   },
 })
+
+// export default defineConfig({
+//   plugins: [react()],
+// })
