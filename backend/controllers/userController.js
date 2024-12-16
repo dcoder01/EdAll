716f10cc35d3b4b3dfe19fb5c0a5767854e9f319
Debajyoti Debnath <debajyotidebnath43783@gmail.com>
2024-12-17 01:31:32 +0530

    prod fix-cookie not getting deleted

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -50,15 +50,10 @@ exports.loginUser = catchAsyncError(async (req, res, next) => {
 //logout
 exports.logout=catchAsyncError(async(req, res, next)=>{
 
-    res.cookie('token', null, {
-        expires:new Date(Date.now()),
-        httpOnly:true
-    })
-
-    res.status(200).json({
-        success:true,
-        message:"logged out"
-    })
+    res.clearCookie("token").status(200).json({
+        success: true,
+        message: "Logged out successfully!",
+      });
 })
 
 
